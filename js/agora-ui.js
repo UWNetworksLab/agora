952f44940c1a2bc527f856de670ae9ee99b324b2
Nicholas Shahan <nshahan@hyperion.hsd1.wa.comcast.net>
2013-02-10 22:49:06 -0800

    working on the file system table display. Not working yet

--- js/agora-ui.js
+++ js/agora-ui.js
@@ -12,9 +12,17 @@ $(".fs-item").click(function () {
 // Get the current Username
 $("#user-name").html(Agora.getCurrentUser().displayName);
 
-/*$("file-system").html(function () {
-	Agora.getSpace
-	for (var i=0; i<???.length; i++) {
-		document.write(cars[i] + "<br>");
-} 
-});*/
+// Print file system table
+/*
+$("file-system").html(function () {
+	Agora.getSpaceByName("Final Project");
+	
+	for (var i=0; i<fs.contents.length; i++) {
+		document.write("<tr>");
+		document.write("<td>"+"This is a test</td>");
+		document.write("<td>directory</td>");
+		document.write("<td>10/20/2012</td>");
+		document.write("</tr>");
+	}
+});
+*/
