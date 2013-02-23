bb02814fa8def773f03ac2a97aff0a2b16c7afcf
Caylan Lee <caylanlee@gmail.com>
2013-02-22 18:16:18 -0800

    playing with rendering, lots to be done still

--- js/agora-ui.js
+++ js/agora-ui.js
@@ -28,13 +28,38 @@ var currentUserView = new UserView({ model: currentUser, el: $("#user-name")})
 
 // File view/template
 var FileView = Backbone.View.extend({
+	tagName: 'li',
 	initialize: function() {
 		this.render();
 	},
 
 	render: function() {
-		var template = _.template( "...", this.model.toJSON() );
+		var template = _.template( "<%= name %>", this.model.toJSON() );
 		this.$el.html(template);
 	}
 
 });
+
+var SpaceView = Backbone.View.extend({
+	initialize: function() {
+		this.render();
+	},
+
+	render: function() {
+		var template = _.template("")
+
+	}
+});
+
+// Get the current space
+var currentSpace = Agora.getSpaceByName("Final Project");
+
+// var currentSpaceView = new SpaceView({ model: currentSpace, el: $("#fileList") });
+
+var fs = currentSpace.get("fileSystem");
+
+
+var file1 = new FileView({ model: fs.at(0), el: $("#fileList") });
+var file2 = new FileView({ model: fs.at(1), el: $("#fileList") });
+
+// $("fileList").html(thisFileViewTerribleName);
