2758e94f6e98b45fa80db97178b763b7b8ae8159
Tariq Yusuf <yusuft@cs.washington.edu>
2013-03-04 16:03:45 -0800

    Started FreeDOM backend, updated compiled JS

--- freedom-api-test/agoraBackend.js
+++ freedom-api-test/agoraBackend.js
@@ -1,16 +1,2 @@
 // helloworld test
 freedom.emit("helloworld", "Hello, World!  This is Agora from FreeDOM!");
-
-// storage test
-function StorageTest() {
-}
-
-StorageTest.prototype.set = function(value) {
-	localStorage.storagetest = value;
-}
-
-StorageTest.prototype.get = function() {
-	return localStorage.storageTest;
-}
-
-freedom.agoraBackend().provideSynchronous(StorageTest);
