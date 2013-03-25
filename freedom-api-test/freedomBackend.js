190235ff849171d8f8df652ce91e8271956d2a3f
Tariq Yusuf <yusuft@cs.washington.edu>
2013-03-25 15:22:35 -0700

    Added FreeDOM API update, added more onto Backbone.sync

--- freedom-api-test/freedomBackend.js
+++ freedom-api-test/freedomBackend.js
@@ -1,5 +1,33 @@
+/**
+ * Provides a CRUD model meant for overriding Backbone.sync
+ *
+ * @module FreeDOM Backbone.sync
+ */
 var storage = freedom.storage();
 
-freedom.on("sync_create", function(model) {
-   console.log("sync_create called");
+// Create/Update model
+freedom.on("sync_update", function(model, modelName, continuation) {
+   var promise = storage.set(modelName + "_" + model.id, model);
+   promise.done(function() {
+      if(continuation != undefined)
+         continuation(model);
+   })
 });
+
+// Read model
+freedom.on("sync_read", function(id, modelName, continuation) {
+   var promise = storage.get(modelName + "_" + id);
+   promise.done(function(val) {
+      if(continuation != undefined)
+         continuation(val);
+   });
+});
+
+// Delete model
+freedom.on("sync_delete", function(id, modelName, continuation) {
+   var promise = storage.set(modelName + "_" + id, {});
+   promise.done(function() {
+      if(continuation != undefined)
+         continuation(model);
+   });
+}
--- freedom-api-test/main.js
+++ freedom-api-test/main.js
@@ -1,21 +1,50 @@
+/**
+ * Overrides the built-in synchronization for Backbone.sync to use
+ * the FreeDOM API.  In order for the synchronization to work properly
+ * two properties need to be provided.
+ *
+ * In the model, a id property needs to be set to prevent synchronization
+ * conflicts.  In addition, a modelName option should be provided in
+ * options to specify the model name.  Excluding any of these properties
+ * will result in an error
+ *
+ * @method Backbone.sync
+ * @param method the sync method to use, a CRUD command
+ * @param model the model to syncrhonize
+ * @param the options to use in synchronizing
+ */
 Backbone.sync = function(method, model, options) {
-   console.info("Backbone.sync: [" + method + ", " + model + ", " + options + "]");
+   // Error handling
+   if(model.id === undefined)
+      throw "Backbone.sync: id not defined in model";
+   if(options.modelName === undefined)
+      throw "Backbone.sync: modelName not defined in options";
+   console.info("Backbone.sync: [" + method.toString() + ", " +
+      JSON.stringify(model) + ", " + JSON.stringify(options) + "]");
 
    switch(method) {
       case "create":
          console.info("Backbone.sync: create called");
+         freedom.emit("sync_update", model, options.modelName);
          break;
       case "read":
          console.info("Backbone.sync: read called");
+         freedom.emit("sync_read", model.id, options.modelName,
+            function(modelResult) {
+               model.attributes = modelResult;
+            }
+         );
          break;
       case "update":
          console.info("Backbone.sync: update called");
+         freedom.emit("sync_update", model, options.modelName);
          break;
       case "delete":
          console.info("Backbone.sync: delete called");
+         freedom.emit("sync_delete", model, options.modelName);
          break;
       default:
-         console.error("Backbone.sync: Invalid method " + method);
+         throw "Backbone.sync: Invalid method " + method;
    }
 };
 
